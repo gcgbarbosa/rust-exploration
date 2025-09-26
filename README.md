b9df63fc846e3353f0a546c7e0dc714a5eade82b
gcgbarbosa <gcgbarbosa@gmail.com>
2025-09-26 21:40:36 +0200

    feat: add random number guessing game
    - Introduced a new guessing game using the `rand` crate to generate a secret number.
    - Updated `Cargo.toml` to include the `rand` dependency.
    - Modified `Cargo.lock` with updated dependencies and checksums.
    - Enhanced `README.md` with the initial step of the project.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## going through [the book](https://doc.rust-lang.org/book)
 
+- [x] 1: hello world
 - [ ] 2: guessing game
-
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use ::std::cmp::Ordering;
+use ::std::io;
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        let mut guess = String::new();
+
+        println!("Please input your guess");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read a line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+        }
+
+        println!("You guessed {guess}");
+    }
 }
